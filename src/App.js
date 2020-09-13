5f7335e13cf95b726edb828c8342f7fda8618a9b
Ojhal Patwa <patkarojhal26@gmail.com>
2020-09-13 20:38:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+// import Button from 'react-bootstrap/Button';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          Login Page
+
+      
+      </div>
+    );
+  }
+}
+
+
+export default App;
