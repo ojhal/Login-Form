3bdc9ce3d7727ba3c3180ef6fd7c897fb7bff144
Ojhal Patwa <patkarojhal26@gmail.com>
2020-09-13 21:59:49 +0530

    Import & export

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 // import Button from 'react-bootstrap/Button';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
+import { FacebookLoginButton } from 'react-social-login-buttons';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          Login Page
-
-      
-      </div>
+      <Form className="App">
+        <h1>mywebsite.com</h1>
+      </Form>
     );
   }
 }
