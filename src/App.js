c5358b2a7ee45b9e92b87a1283573316384a3be9
Ojhal Patwa <patkarojhal26@gmail.com>
2020-09-13 22:38:14 +0530

    add css

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import { FacebookLoginButton } from 'react-social-login-buttons';
 class App extends Component {
   render() {
     return (
-      <Form className="App">
+      <Form className="login-form">
         <h1>
           <span className="font-weight-bold">mywebsite</span>.com
         </h1>
         <h2>Welcome</h2>
-        <label>Email</label>
+        <FormGroup>
+          <label>Email</label>
+          <Input type="email" placeholder="Email" />
+        </FormGroup>
       </Form>
     );
   }
